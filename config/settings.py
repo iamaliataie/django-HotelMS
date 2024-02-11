4cb7b2b6e1227e837db6246c6c74c1352dab6366
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-11 20:03:26 +0430

    jazzmin dashboard settings

--- config/settings.py
+++ config/settings.py
@@ -145,5 +145,6 @@ JAZZMIN_SETTINGS = {
     'topmenu_links': [
         {'name': 'Home', 'url': 'admin:index', 'permission': ['auth.view_user']},
         {'name': 'Company', 'url': '/admin/addons/company/'},
+        {'name': 'Users', 'url': '/admin/userauth/uer/'}
     ]
 }
