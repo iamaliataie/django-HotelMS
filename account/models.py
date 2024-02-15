bec4aedfd5333bb905ed7543d6bef46997c98cbe
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-15 21:30:21 +0430

    update account models

--- account/models.py
+++ account/models.py
@@ -66,3 +66,6 @@ class Profile(models.Model):
     
 def create_user_profile(sender, instance, created, **kwargs):
     if created: Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
