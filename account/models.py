2f0583b164625738c6d0d7542b54fa465885660f
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-15 20:46:43 +0430

    update account models

--- account/models.py
+++ account/models.py
@@ -7,3 +7,4 @@ class User(AbstractUser):
     full_name = models.CharField(max_length=500, null=True, blank=True)
     username = models.CharField(max_length=500, unique=True)
     email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
