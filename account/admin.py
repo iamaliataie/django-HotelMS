da2728dde4fe7a23f471e7a85bab14c6a6a26f16
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-15 21:56:00 +0430

    account admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 from .models import User, Profile
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['full_name', 'username']
