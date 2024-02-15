d709561524b9eef9686fef36ccbe41585987facd
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-15 21:11:28 +0430

    update account models

--- account/models.py
+++ account/models.py
@@ -40,3 +40,4 @@ class Profile(models.Model):
     country = models.CharField(max_length=100, null=True, blank=True)
     city = models.CharField(max_length=100, null=True, blank=True)
     state = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=1000, null=True, blank=True)
