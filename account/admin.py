7376e3325f1cfe34dfaecd6baea62fc66cac2d6e
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-15 22:04:03 +0430

    account admin

--- account/admin.py
+++ account/admin.py
@@ -8,7 +8,7 @@ class UserAdmin(admin.ModelAdmin):
 
 class ProfileAdmin(admin.ModelAdmin):
     search_fields = ['full_name', 'user__username']
-    list_display = ['full_name', 'user', 'verified']
+    list_display = ['pid', 'full_name', 'user', 'verified']
 
-admin.site.regiser(User, UserAdmin)
-admin.site.regiser(Profile, ProfileAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
