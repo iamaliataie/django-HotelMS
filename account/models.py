2127b79cd7e564edbcdca2c25d65c8f2221838a7
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-15 21:26:39 +0430

    update account models

--- account/models.py
+++ account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.db.models.signals import post_save
 from shortuuid.django_fields import ShortUUIDField
 # Create your models here.
 
