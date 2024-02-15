ec23b7d758eaaa239ab9f1afac7f9cb6e61d0bda
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-15 20:46:12 +0430

    update account models

--- account/models.py
+++ account/models.py
@@ -6,3 +6,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     full_name = models.CharField(max_length=500, null=True, blank=True)
     username = models.CharField(max_length=500, unique=True)
+    email = models.EmailField(unique=True)
