d60f2d90e5ff20e2a678ed364ffdacad903d5e02
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-16 21:25:36 +0430

    account updated

--- account/views.py
+++ account/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+
+
 from account.models import User, Profile
 from account.forms import UserRegistrationForm
 # Create your views here.
 
 def register(request):
     form = UserRegistrationForm()
+    
+    if form.is_valid():
+
     context = {
         'form': form
     }
