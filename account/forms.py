364eaf5e3dacb07e7a876f308abfec8a796fb43c
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-19 21:00:10 +0430

    updated account forms

--- account/forms.py
+++ account/forms.py
@@ -4,6 +4,7 @@ from .models import User, Profile
 
 class UserRegistrationForm(UserCreationForm):
     full_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter full name'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     class Meta:
         model = User
         fields = ['full_name', 'username', 'phone', 'email', 'password1', 'password2']
