09b6b9a24656131bf8399e5fb1c5be5198e926c6
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-19 21:02:16 +0430

    updated account forms

--- account/forms.py
+++ account/forms.py
@@ -8,6 +8,7 @@ class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email Address'}))
     phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Phone Number'}))
     passowrd1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    passowrd2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
     class Meta:
         model = User
         fields = ['full_name', 'username', 'phone', 'email', 'password1', 'password2']
