dac4e80ef6eb1883792e0cb13e9c68448075ad54
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-19 20:54:29 +0430

    updated settings

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'templates/',
+]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
