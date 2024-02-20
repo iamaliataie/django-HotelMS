212a3c08e78dfe04ba99c39e6d94e343e7fbfc95
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-20 22:04:34 +0430

    account view update

--- account/views.py
+++ account/views.py
@@ -51,7 +51,7 @@ def login(request):
                 return redirect(next_url)
             else:
                 messages.error(request, 'Username or password does not exist')
-                return
+                return redirect()
         except:
             messages.error(request, 'Username or password does not exist')
             return redirect('hotel:home')
