b713e141904e758e320191be3c41db09eca28474
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-22 20:16:32 +0430

    hotel model update

--- hotel/models.py
+++ hotel/models.py
@@ -20,3 +20,4 @@ class Hotel(models.Model):
     mobile = models.CharField(max_length=200)
     email = models.EmailField(max_length=100)
     status = models.CharField(max_length=20, choices=HOTEL_STATUS, default='live')
+    tags = models.CharField()
