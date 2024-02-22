dc06eb1d673dc76ac96237b0a8521531af967107
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-22 20:08:02 +0430

    hotel model update

--- hotel/models.py
+++ hotel/models.py
@@ -3,4 +3,4 @@ from account.models import User
 # Create your models here.
 
 class Hotel(models.Model):
-    user = models.ForeignKey(User,)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
