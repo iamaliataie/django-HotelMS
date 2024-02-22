108659c66184816745b57df779de93e122233031
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-22 20:12:52 +0430

    hotel model update

--- hotel/models.py
+++ hotel/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from account.models import User
 # Create your models here.
 
-HOTEL_STATUS = ()
+HOTEL_STATUS = (
+    ('draft', 'Draft')
+)
 
 class Hotel(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
