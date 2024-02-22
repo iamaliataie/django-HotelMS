08f64d97521d796eb1ed22aff8bac75bbba04405
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-22 20:09:23 +0430

    hotel model update

--- hotel/models.py
+++ hotel/models.py
@@ -6,3 +6,4 @@ class Hotel(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL)
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    image = models.FileField()
