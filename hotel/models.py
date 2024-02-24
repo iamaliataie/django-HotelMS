d6be6619bb0fc0de03d42dad7fac30a5edc2d4f9
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-24 19:47:07 +0430

    update hotel models

--- hotel/models.py
+++ hotel/models.py
@@ -156,4 +156,4 @@ class Booking(models.Model):
     full_name = models.CharField(max_length=1000)
     email = models.EmailField(max_length=1000)
     phone = models.CharField(max_length=1000)
-    hotel = models.ForeignKey()
+    hotel = models.ForeignKey(Hotel, on_delete=models.SET_NULL)
