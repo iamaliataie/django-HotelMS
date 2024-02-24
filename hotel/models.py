9aca0a069c31c9475a7f7a878b34a8a57930c5f2
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-24 20:03:07 +0430

    update hotel models

--- hotel/models.py
+++ hotel/models.py
@@ -173,4 +173,4 @@ class Booking(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     stripe_payment_intent = models.CharField(max_length=1000, null=True, blank=True)
     success_id = models.CharField(max_length=1000, null=True, blank=True)
-    booking_id = ShortUUIDField(unique=True,, length=10)
+    booking_id = ShortUUIDField(unique=True, length=10, max_length=20)
