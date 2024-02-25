0e83ea7afde9d45ed409ea9e02104ac80bb0eb55
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-25 20:34:38 +0430

    update hotel models

--- hotel/models.py
+++ hotel/models.py
@@ -188,3 +188,6 @@ class ActivityLog(models.Model):
     guess_in = models.DateTimeField()
     description = models.TextField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.booking}'
