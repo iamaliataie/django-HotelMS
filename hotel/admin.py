25ad31804b5c338738896532fa65d9521b4c8e29
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-02-29 20:29:10 +0430

    UPdate hotel veiws

--- hotel/admin.py
+++ hotel/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Hotel, Booking, ActivityLog, StaffOnDuty, Room, RoomType, HotelGallery
 # Register your models here.
 
+
+class HotelGalleryInline(admin.TabularInline):
+
 class HotelAdmin(admin.ModelAdmin):
     list_display = ['thumbnail', 'name', 'user', 'status']
     prepopulated_fields = {'slug': ('name',)}
