d703aa09368bf16b97771403d597f307564cfe35
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-01 19:25:35 +0430

    update hotel views

--- hotel/urls.py
+++ hotel/urls.py
@@ -6,5 +6,5 @@ app_name = 'hotel'
 urlpatterns = [
     path('', views.index, name='home'),
     path('detail/<slug>/', views.hotel_detail, name='hotel_detail'),
-    path('detail/<slug:slug>/room-type/<slug:rt_slug>/', views.room_type_detail, name=''),
+    path('detail/<slug:slug>/room-type/<slug:rt_slug>/', views.room_type_detail, name='room_type_detail'),
 ]
