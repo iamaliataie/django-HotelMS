c679eccb94d1c6f2e555289465e9001d5dbdaf4a
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-03 20:03:01 +0430

    update booking views

--- booking/urls.py
+++ booking/urls.py
@@ -3,3 +3,11 @@ from django.urls import path
 from . import views
 
 app_name = 'booking'
+
+urlpatterns = [
+    path(
+        'check_room_availability/',
+        VIEW.as_view(),
+        name=''
+    ),
+]
