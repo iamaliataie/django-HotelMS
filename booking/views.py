4b38bad509126b32ba451724d18eeee507b5a47b
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-03 19:50:54 +0430

    update booking views

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, render, redirect
 from django.urls import reverse
-from hotel.models import Hotel, Booking, ActivityLog, HotelGallery
+from hotel.models import Hotel, Booking, ActivityLog, HotelGallery, StaffOnDuty
 # Create your views here.
