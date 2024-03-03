e721e21df4d0b3168894d9eb060907475687ab13
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-03 19:51:24 +0430

    update booking views

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, render, redirect
 from django.urls import reverse
-from hotel.models import Hotel, Booking, ActivityLog, HotelGallery, StaffOnDuty
+from hotel.models import (
+    Hotel, 
+    Booking, 
+    ActivityLog,
+    HotelGallery,
+    StaffOnDuty
+)
 # Create your views here.
