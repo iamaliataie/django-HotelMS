de0c5f6ea06e1696f0ca8cb27730fdeda4b69862
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-07 21:15:09 +0430

    Update views.py

--- booking/views.py
+++ booking/views.py
@@ -23,7 +23,6 @@ def check_room_availability(request):
         adult = request.POST.get('adult')
         children = request.POST.get('children')
         room_type = request.POST.get('room_type')
-
         hotel = Hotel.objects.get(id=id)
         room_type = RoomType.objects.get(hotel=hotel, slug=room_type)
 
