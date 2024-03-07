9d07fb5c3d6801e0a8c950a58f8158f7953845c0
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-07 21:16:53 +0430

    Update views.py

--- hotel/views.py
+++ hotel/views.py
@@ -14,7 +14,6 @@ def hotel_detail(request, slug):
     context = {
         'hotel': hotel
     }
-    
     return render(request, 'hotel/hotel_detail.html', context)
 
 def room_type_detail(request, slug, rt_slug):
