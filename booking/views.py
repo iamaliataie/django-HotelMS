40b8af8f26c2a9a1edc7191671d51d153551042f
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-07 21:14:57 +0430

    Update views.py

--- booking/views.py
+++ booking/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, render, redirect
 from django.urls import reverse
 from django.http import HttpResponseRedirect, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
 from hotel.models import (
     Hotel, 
     Booking, 
