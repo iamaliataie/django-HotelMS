acb162c83805d7ec1139b9bd2047a6877ff7b348
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-08 20:39:20 +0430

    Update models.py

--- hotel/models.py
+++ hotel/models.py
@@ -53,7 +53,6 @@ class Hotel(models.Model):
     slug = models.SlugField(unique=True)
     date = models.DateTimeField(auto_now_add=True)
 
-    
     def __str__(self):
         return self.name
     
