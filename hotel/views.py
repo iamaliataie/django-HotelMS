950916fa68f5218679a718fd635054f4d3815a55
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-14 20:54:03 +0430

    update hotel views

--- hotel/views.py
+++ hotel/views.py
@@ -51,6 +51,7 @@ def selectd_rooms(request):
             for h_id, item in request.session['selection_data_obj'].items():
                 id = int(item['hotel_id'])
                 checkin = item['checkin']
+                checkout = item['checkout']
         for h_id, item in request.session['selection_data_obj'].items():
             id = int(item['hotel_id'])
             checkin = item['checkin']
