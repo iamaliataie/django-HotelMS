378f0cbf12ce0a05aaff5402bcbe3c0143cab979
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-14 20:55:48 +0430

    update hotel views

--- hotel/views.py
+++ hotel/views.py
@@ -65,6 +65,7 @@ def selectd_rooms(request):
             checkout_date = datetime.strptime(checkout, date_format)
             time_difference = checkout_date - checkin_date
             total_days = time_difference.days
+            full_name = request.POST.get('full_name')
         for h_id, item in request.session['selection_data_obj'].items():
             id = int(item['hotel_id'])
             checkin = item['checkin']
