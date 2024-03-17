dfa6c91331a4a19f85ba4e26a04ba90c8e7a636e
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-17 21:11:49 +0430

    Update views.py

--- hotel/views.py
+++ hotel/views.py
@@ -5,7 +5,6 @@ from .models import Hotel, Booking, ActivityLog, RoomType, Room
 
 def index(request):
     hotels = Hotel.objects.filter(status='live')
-    
     context = {
         'hotels': hotels
     }
