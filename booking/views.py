f9ca6546e17fd98a16ca940b02112e9c5fd626aa
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-03-22 20:48:50 +0430

    update booking views

--- booking/views.py
+++ booking/views.py
@@ -98,3 +98,5 @@ def delet_selection(request):
             checkout_date = datetime.strptime(checkout, date_format)
             time_difference = checkout_date - checkin_date
             total_days = time_difference.days
+            
+            room_count += 1
